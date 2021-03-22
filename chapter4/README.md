8bae412e1aaa450d554aeea7998b8e17e808d330
boris <baboldyshev@gmail.com>
2021-03-22 17:14:04 +0300

    chapter 4 final

--- chapter4/README.md
+++ chapter4/README.md
@@ -5,6 +5,32 @@
 [fig4_1.py](fig4_1.py), [fig4_1.ipynb](fig4_1.ipynb)
 
 # Figure 4.2
+Detailed analysis of the problem in Aditya Rastogi's blogpost 
+[Elucidating Policy Iteration in Reinforcement Learning — Jack’s Car Rental Problem](https://towardsdatascience.com/elucidating-policy-iteration-in-reinforcement-learning-jacks-car-rental-problem-d41b34c8aec7)
+
+His repo https://github.com/thunderInfy/JacksCarRental
+
+Good ideas how to speed up code execution in Yang Zhang's repo https://github.com/zy31415/jackscarrental
 
 ![fig4_2](figs/fig4_2.svg)
-[fig4_2.py](fig4_2.py), [fig4_2.ipynb](fig4_2.ipynb)
+[fig4_2.py](fig4_2.py), for explanations please see jupyter notebook [fig4_2.ipynb](fig4_2.ipynb)
+
+# Figure 4.3
+An excellent explanation on the family of optimal policies in vojtamolda's [repo](https://github.com/vojtamolda/reinforcement-learning-an-introduction/blob/main/chapter04/chapter04.pdf)
+
+How to replicate policy plot in the book https://github.com/ShangtongZhang/reinforcement-learning-an-introduction/issues/83
+
+Some experiments with policies in James Teow's blog https://medium.com/@jaems33/gamblers-problem-b4e91040e58a
+
+![fig4_3](figs/fig4_3.svg)
+[fig4_3.py](fig4_3.py), [fig4_3.ipynb](fig4_3.ipynb)
+
+# Exercise 4.9
+p = 0.25
+![ex4_9a](figs/ex4_9a.svg)
+
+p = 0.55
+![ex4_9b](figs/ex4_9b.svg)
+
+Code is the same as for figure 4.3 ([fig4_3.py](fig4_3.py)).
+
--- chapter4/fig4_3.py
+++ chapter4/fig4_3.py
@@ -2,71 +2,131 @@
 
 """Figure 4.3, page 84"""
 
-import random
 import copy
-import matplotlib.pyplot as plt
+
 import numpy as np
-PROBABILITY_HEADS = 0.4
-HEADS = True
-TAILS = False
+import matplotlib.pyplot as plt
+
+# probability to win the stake
+PROBABILITY_HEADS = 0.25
+
+# states with $0 or $100 are not considered as the are terminal
+STATES = np.arange(1, 100)
+
+# accuracy
 THETA = 1e-9
-STATES = np.arange(101)
-VALUES_HISTORY_STEPS = (1, 2, 3, 32)
 
 values = np.zeros(101)
 values[100] = 1
 policy = np.zeros(101)
 
 
-def expected_value(values, state, action):
+def actions(state):
+    """Get possible actions for a given state
+
+    :param state: State
+    :type state: int
+    :return: Possible actions
+    :rtype: numpy array
+    """
+    return np.arange(1, min(state, 100 - state) + 1)
+
+
+def expected_update(values, state, action):
+    """Compute expected value for given state and action
+
+    :param values: Current values
+    :type: numpy array
+    :param state: State
+    :type: int
+    :param action: Action
+    :type: int
+    :return: expected value
+    :rtype: float
+    """
     return PROBABILITY_HEADS * values[state + action] + (1 - PROBABILITY_HEADS) * values[state - action]
 
 
+def policy_evaluate():
+    """Policy evaluation
+
+    :return: Difference between old and new value
+    :rtype: float
+    """
+    delta = 0
+    for state in STATES:
+        old_value = values[state]
+        values[state] = max([expected_update(values, state, action) for action in actions(state)])
+        delta = max(delta, abs(old_value - values[state]))
+    return delta
+
+
+def policy_improve(round_digits=4):
+    """Policy improvement
+
+    :param round_digits: Precision of expected value
+    :type round_digits: int
+    :return: None
+    :rtype: None
+    """
+    for state in STATES:
+        _actions = actions(state)
+        action_values = [round(expected_update(values, state, action), round_digits) for action in _actions]
+        optimal_action = _actions[np.argmax(action_values)]
+        policy[state] = optimal_action
+
+
 def value_iteration():
-    values_history = list()
-    iteration = 0
+    """Value iteration algorithm implementation (page 83)
+
+    :return: Values after each sweep of policy evaluation
+    :rtype: list
+    """
+    values_history = [values]
+
+    sweep = 0
     delta = 1
     while delta >= THETA:
-        if iteration in VALUES_HISTORY_STEPS:
-            values_history.append(copy.deepcopy(values))
-        delta = 0
-        for state in STATES[1:100]:
-            actions = np.arange(min(state, 100 - state) + 1)
-
-            old_value = values[state]
-            values[state] = max([expected_value(values, state, action) for action in actions])
-            delta = max(delta, abs(old_value - values[state]))
-        iteration += 1
-    values_history.append(values)
+        delta = policy_evaluate()
+        sweep += 1
+        values_history.append(copy.deepcopy(values))
     return values_history
 
 
-def policy_improve():
-    for state in STATES[1:100]:
-        actions = np.arange(min(state, 100 - state) + 1)
-        action_values = [expected_value(values, state, action) for action in actions]
-        policy[state] = actions[np.argmax(np.round(action_values[1:], 5)) + 1]
+def plot(values_history, policy):
+    """Plot values and policy
 
-
-def fig4_3():
-    values_history = value_iteration()
-    policy_improve()
+    :param values_history: Values after each sweep of policy evaluation
+    :type values_history: list
+    :param policy: Policy
+    :type policy: numpy array
+    :return: None
+    :rtype: None
+    """
     plt.figure(figsize=(10, 20))
-
     plt.subplot(2, 1, 1)
-    for sweep, _value in zip(VALUES_HISTORY_STEPS, values_history):
-        plt.plot(_value, label='sweep {}'.format(sweep))
+
+    values_history_sweeps = (1, 2, 3, len(values_history)//2)
+    for sweep in values_history_sweeps:
+        plt.plot(values_history[sweep], label=f'sweep {sweep}')
     plt.plot(values, label='sweep final')
+
     plt.xlabel('Capital')
     plt.ylabel('Value estimates')
     plt.legend(loc='best')
 
     plt.subplot(2, 1, 2)
-    plt.bar(STATES, policy)
+    plt.bar(np.arange(len(policy)), policy)
     plt.xlabel('Capital')
     plt.ylabel('Final policy (stake)')
 
     plt.show()
+
+
+def fig4_3():
+    values_history = value_iteration()
+    policy_improve()
+    plot(values_history, policy)
 
 
 if __name__ == '__main__':
