a0e1108d47b6e37f08f13ded859c18f358ae5add
boris <baboldyshev@gmail.com>
2021-04-04 23:57:06 +0300

    chapter 5

--- chapter5/fig5_1.py
+++ chapter5/fig5_1.py
@@ -12,67 +12,40 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# diamonds, clubs, hearts and spades
-SUITS_NUMBER = 4
-
-# jacks, queens and kings
-FACE_CARDS_NUMBER = 3
-
-# All scores above 21 counted as 0
-FINAL_SCORES = tuple(itertools.product((0, 20, 21), list(range(17, 22)) + [0]))
-
-# Precalculate rewards for possible outcomes
-REWARDS = dict()
-for score_player, score_dealer in FINAL_SCORES:
-    if score_player > score_dealer:
-        reward = 1
-    elif score_player == score_dealer:
-        reward = 0
-    else:
-        reward = -1
-    REWARDS[score_player, score_dealer] = reward
-
-# Fill the deck of cards
-DECK = list()
-for card in range(1, 11):
-    DECK += [card] * SUITS_NUMBER
-
-# Fill the deck with face cards
-DECK += [10] * SUITS_NUMBER * FACE_CARDS_NUMBER
-
 
 class BlackJack:
+    # diamonds, clubs, hearts and spades
+    SUITS_NUMBER = 4
 
-    def __init__(self, deck):
+    # jacks, queens and kings
+    FACE_CARD_TYPES = 3
 
-        # Create the deck
-        self.deck = copy.copy(deck)
+    # Fill the deck with aces (1) and pip cards (2-10)
+    _deck = [card for card in range(1, 11)] * SUITS_NUMBER
+
+    # Fill the deck with face cards
+    _deck += [10] * SUITS_NUMBER * FACE_CARD_TYPES
+
+    def __init__(self):
+
+        # New deck for each game
+        self.deck = copy.copy(self._deck)
 
         # Begin game: deal two cards for dealer and player
-        self.hand_player = [self.deal_card() for _ in range(2)]
-        self.hand_dealer = [self.deal_card() for _ in range(2)]
-
-        # Check if player has usable ace
-        _usable_ace = self.usable_ace(self.hand_player)
+        self.player_hand = [self.deal_card() for _ in range(2)]
+        self.dealer_hand = [self.deal_card() for _ in range(2)]
 
         # Put one of the dealer's cards face up
-        self.visible_dealer_card = self.hand_dealer[0]
+        self.dealer_showing = self.dealer_hand[0]
 
-        # Compute the scores
-        self.score_dealer, self.hand_dealer = self.score(self.hand_dealer)
-        self.score_player, self.hand_player = self.score(self.hand_player)
+        # Compute the sums
+        usable_ace, self.player_sum, self.player_hand = self.count_sum(self.player_hand)
+        _, self.dealer_sum, self.dealer_hand = self.count_sum(self.dealer_hand)
 
-        # Initial state
-
-        # Player always hits if his score is below 12
-        while self.score_player < 12:
-            card = self.deal_card()
-            self.hand_player.append(card)
-            self.score_player, self.hand_player = self.score(self.hand_player)
-        self.player_states = [(self.score_player, self.visible_dealer_card, _usable_ace)]
+        self.initial_player_state = usable_ace, self.player_sum, self.dealer_showing
 
     def deal_card(self):
-        """Take a random card out of the deck"""
+        """Take a random card from the deck"""
         return self.deck.pop(random.randrange(len(self.deck)))
 
     @staticmethod
@@ -81,97 +54,135 @@ class BlackJack:
         usable = sum(hand) < 12
         return ace and usable
 
-    def score(self, hand):
-        """Compute the score of a given hand"""
+    def count_sum(self, hand):
+        """Compute the sum of a given hand"""
         new_hand = copy.copy(hand)
-        _score = sum(hand)
-        _usable_ace = self.usable_ace(hand)
-        _used_ace = 11 in hand
-        if _usable_ace:
+        _sum = sum(hand)
+        usable_ace = self.usable_ace(hand)
+        used_ace = 11 in hand
+        if usable_ace:
             ace_index = new_hand.index(1)
             new_hand[ace_index] = 11
-            return _score + 10, new_hand
-        elif _score > 21 and _used_ace:
+            _sum += 10
+        elif _sum > 21 and used_ace:
             ace_index = new_hand.index(11)
             new_hand[ace_index] = 1
-            return _score - 10, new_hand
-        elif _score > 21:
-            return 0, new_hand
-        return _score, new_hand
+            _sum -= 10
+        return usable_ace, _sum, new_hand
 
     def play_as_dealer(self):
-        while 2 < self.score_dealer < 17:
+        while 2 < self.dealer_sum < 17:
             card = self.deal_card()
-            self.hand_dealer.append(card)
-            self.score_dealer, self.hand_dealer = self.score(self.hand_dealer)
+            self.dealer_hand.append(card)
+            _, self.dealer_sum, self.dealer_hand = self.count_sum(self.dealer_hand)
 
-    def step_as_player(self):
+    def player_hits(self):
         card = self.deal_card()
-        self.hand_player.append(card)
-        _usable_ace = self.usable_ace(self.hand_player)
-        self.score_player, self.hand_player = self.score(self.hand_player)
-        new_state = self.score_player, self.visible_dealer_card, _usable_ace
-        self.player_states.append(new_state)
+        self.player_hand.append(card)
+        usable_ace, self.player_sum, self.player_hand = self.count_sum(self.player_hand)
+        new_state = usable_ace, self.player_sum, self.dealer_showing
         return new_state
 
 
-def plot_values(values_usable_ace, values_no_usable_ace):
+def generate_episode(policy):
+    black_jack = BlackJack()
+    player_state = black_jack.initial_player_state
+    state_sequence = [player_state]
+
+    if 21 in (black_jack.player_sum, black_jack.dealer_sum):
+        if black_jack.player_sum == black_jack.dealer_sum:
+            reward = 0
+        elif black_jack.player_sum == 21:
+            reward = 1
+        elif black_jack.dealer_sum == 21:
+            reward = -1
+            if black_jack.player_sum < 12:
+                state_sequence = list()
+        return state_sequence, reward
+
+    while black_jack.player_sum < 12:
+        player_state = black_jack.player_hits()
+        state_sequence = [player_state]
+        if black_jack.player_sum == 21:
+            reward = 1
+            return state_sequence, reward
+
+    while policy[player_state]:
+        player_state = black_jack.player_hits()
+        if black_jack.player_sum > 21:
+            reward = -1
+            return state_sequence, reward
+        elif black_jack.player_sum == 21:
+            reward = 1
+            state_sequence.append(player_state)
+            return state_sequence, reward
+
+        state_sequence.append(player_state)
+
+    black_jack.play_as_dealer()
+    if black_jack.dealer_sum == 21:
+        reward = -1
+        return state_sequence, reward
+    elif black_jack.dealer_sum > 21:
+        reward = 1
+        return state_sequence, reward
+
+    unequal_sum = black_jack.player_sum != black_jack.dealer_sum
+    reward = int(unequal_sum)
+
+    return state_sequence, reward
+
+
+def plot_values(values):
+    values_arr = np.zeros((2, 10, 10))
+    for (x, y, z), value in values.items():
+        x, y, z = int(x), y - 12, z - 1
+        values_arr[x, y, z] = value
     fig, axes = plt.subplots(2, 1, figsize=(6, 9))
     axes = axes.flatten()
 
-    dealer_states = range(1, 11)
-    player_states = range(12, 22)
+    dealer_showing = range(1, 11)
+    player_sum = range(12, 22)
 
-    h1 = sns.heatmap(values_usable_ace, ax=axes[0], xticklabels=dealer_states, yticklabels=player_states)
+    h1 = sns.heatmap(values_arr[1, :, :], ax=axes[0], xticklabels=dealer_showing, yticklabels=player_sum)
     h1.invert_yaxis()
-    h1.set_ylabel("Usable ace\nplayer's sum")
-    h1.set_xlabel("dealer's card")
+    h1.set_ylabel("Usable ace\nPlayer sum")
+    h1.set_xlabel("Dealer showing")
 
-    h2 = sns.heatmap(values_no_usable_ace, ax=axes[1], xticklabels=dealer_states, yticklabels=player_states)
+    h2 = sns.heatmap(values_arr[0, :, :], ax=axes[1], xticklabels=dealer_showing, yticklabels=player_sum)
     h2.invert_yaxis()
-    h2.set_ylabel("No usable ace\nplayer's sum")
-    h2.set_xlabel("dealer's card")
+    h2.set_ylabel("No usable ace\nPlayer sum")
+    h2.set_xlabel("Dealer showing")
     plt.show()
 
 
-def fig5_1():
+def first_visit_monte_carlo_prediction():
+    usable_ace = (True, False)
+    player_sums = range(12, 22)
+    dealer_showing = range(1, 11)
+    states = tuple(itertools.product(usable_ace, player_sums, dealer_showing))
+
+    values = dict((state, 0) for state in states)
+    returns = dict((state, []) for state in states)
+    policy = dict(((x, y, z), False) if y in (20, 21) else ((x, y, z), True) for (x, y, z) in states)
+
     t0 = time.perf_counter()
-    episodes_number = int(5e5)
-
-    values_usable_ace = np.zeros((10, 10))
-    values_no_usable_ace = np.zeros((10, 10))
-
-    returns_usable_ace = collections.defaultdict(list)
-    returns_no_usable_ace = collections.defaultdict(list)
+    episodes_number = int(1e4)
 
     for i in range(episodes_number):
         print('\r', f'Episode {i + 1}', end='')
-        black_jack = BlackJack(DECK)
-        while 2 < black_jack.score_player < 20 and black_jack.score_dealer <= 21:
-            black_jack.step_as_player()
-        if black_jack.score_player < 22:
-            black_jack.play_as_dealer()
-            _reward = REWARDS[black_jack.score_player, black_jack.score_dealer]
-        else:
-            _reward = 0
+        state_sequence, reward = generate_episode(policy)
+        for state in state_sequence:
+            returns[state].append(reward)
 
-        for state in set(black_jack.player_states):
-            player_sum, visible_card, usable_ace = state
-            if usable_ace:
-                returns_usable_ace[player_sum, visible_card].append(_reward)
-            else:
-                returns_no_usable_ace[player_sum, visible_card].append(_reward)
-
-    for player_sum, visible_card in itertools.product(range(12, 22), range(1, 11)):
-        x, y = player_sum - 12, visible_card - 1
-        values_usable_ace[x, y] = np.array(returns_usable_ace[player_sum, visible_card]).mean()
-        values_no_usable_ace[x, y] = np.array(returns_no_usable_ace[player_sum, visible_card]).mean()
+    for state in states:
+        values[state] = np.array(returns[state]).mean()
 
     t1 = time.perf_counter()
     print(f'\nDone in {t1 - t0} s')
 
-    plot_values(values_usable_ace, values_no_usable_ace)
+    plot_values(values)
 
 
 if __name__ == '__main__':
-    fig5_1()
+    first_visit_monte_carlo_prediction()
