f751df7700b74f4a68023fe91bb9e9ea98e47d8d
boris <baboldyshev@gmail.com>
2021-04-08 20:06:46 +0300

    chapter 5 comments

--- chapter5/fig5_1.py
+++ chapter5/fig5_1.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 
 
 class BlackJack:
+    """Class for simulating the blackjack game episodes"""
 
     # diamonds, clubs, hearts and spades
     SUITS_NUMBER = 4
@@ -26,6 +27,7 @@ class BlackJack:
     # Fill the deck with face cards
     _deck += [10] * SUITS_NUMBER * FACE_CARD_TYPES
 
+    # precalculate rewards for possible game outcomes
     player_final_sums = range(4, 32)
     dealer_final_sums = range(4, 32)
 
@@ -51,6 +53,7 @@ class BlackJack:
         self.deck = copy.copy(self._deck)
 
         # Begin game: deal two cards for dealer and player
+        # "Hand" is a list of cards that player or dealer hold
         self.player_hand = [self.deal_card() for _ in range(2)]
         self.dealer_hand = [self.deal_card() for _ in range(2)]
 
@@ -61,41 +64,52 @@ class BlackJack:
         usable_ace, self.player_sum, self.player_hand = self.count_sum(self.player_hand)
         _, self.dealer_sum, self.dealer_hand = self.count_sum(self.dealer_hand)
 
+        # Initial player state
         self.initial_player_state = usable_ace, self.player_sum, self.dealer_showing
 
     def deal_card(self):
-        """Take a random card from the deck"""
+        """Take a random card from the deck. Taken cards do not return to the deck"""
         return self.deck.pop(random.randrange(len(self.deck)))
 
     @staticmethod
     def usable_ace(hand):
+        """Whether there is a usable ace in a hand"""
         ace = 1 in hand
         usable = sum(hand) < 12
         return ace and usable
 
     def count_sum(self, hand):
         """Compute the sum of a given hand"""
+        # Ace may be represented as 1 or 11
         new_hand = copy.copy(hand)
         _sum = sum(hand)
         usable_ace = self.usable_ace(hand)
         used_ace = 11 in hand
+
         if usable_ace:
+            # Change 1 to 11
             ace_index = new_hand.index(1)
             new_hand[ace_index] = 11
+            # Add 10 points
             _sum += 10
         elif _sum > 21 and used_ace:
+            # Change 11 to 1
             ace_index = new_hand.index(11)
             new_hand[ace_index] = 1
+            # Subtract 10 points
             _sum -= 10
+
         return usable_ace, _sum, new_hand
 
     def play_as_dealer(self):
+        """Dealer hits if he has less than 17"""
         while 2 < self.dealer_sum < 17:
             card = self.deal_card()
             self.dealer_hand.append(card)
             _, self.dealer_sum, self.dealer_hand = self.count_sum(self.dealer_hand)
 
     def player_hits(self):
+        """Single hit move from the player"""
         card = self.deal_card()
         self.player_hand.append(card)
         usable_ace, self.player_sum, self.player_hand = self.count_sum(self.player_hand)
@@ -104,31 +118,58 @@ class BlackJack:
 
 
 def generate_episode(policy):
+    """Single episode simulation
+
+    :param policy: Player's policy. Policy is a dictonary with keys being the player's states
+    and values being True or False for Hit and Stick respectively
+    :type policy: dict
+    :return: Sequence of states and rewards
+    :rtype: tuple
+    """
+
+    # Initialize episode
     black_jack = BlackJack()
     player_state = black_jack.initial_player_state
     state_sequence = [player_state]
 
+    # Player allways hits below 12
     while black_jack.player_sum < 12:
         player_state = black_jack.player_hits()
         state_sequence = [player_state]
 
+    # Player's move
     while policy[player_state]:
         player_state = black_jack.player_hits()
 
+        # Stop the game if the player goes bust
         if black_jack.player_sum > 21:
             return state_sequence, black_jack.rewards[black_jack.player_sum, black_jack.dealer_sum]
+
+        # Add new state to sequence only if the player hadn't gone bust
         state_sequence.append(player_state)
 
+    # Dealer's move
     black_jack.play_as_dealer()
 
     return state_sequence, black_jack.rewards[black_jack.player_sum, black_jack.dealer_sum]
 
 
 def plot_values(values):
+    """Plot values heatmap
+
+    :param values: State value function
+    :type values: dict
+    :return: None
+    :rtype: None
+    """
+
+    # Convert dict to numpy array
     values_arr = np.zeros((2, 10, 10))
     for (x, y, z), value in values.items():
         x, y, z = int(x), y - 12, z - 1
         values_arr[x, y, z] = value
+
+    # plotting
     fig, axes = plt.subplots(2, 1, figsize=(6, 9))
     axes = axes.flatten()
 
@@ -148,9 +189,13 @@ def plot_values(values):
 
 
 def first_visit_monte_carlo_prediction():
+    """Implementation of the First-visit MC prediction algorithm, page 92"""
+
+    # Initialize
     usable_ace = (True, False)
     player_sums = range(12, 22)
     dealer_showing = range(1, 11)
+
     states = tuple(itertools.product(usable_ace, player_sums, dealer_showing))
 
     values = dict((state, 0) for state in states)
@@ -160,12 +205,15 @@ def first_visit_monte_carlo_prediction():
     t0 = time.perf_counter()
     episodes_number = int(5e5)
 
+    # Loop over episodes
     for i in range(episodes_number):
         print('\r', f'Episode {i + 1}', end='')
         state_sequence, reward = generate_episode(policy)
+        # Loop for each step of episode
         for state in state_sequence:
             returns[state].append(reward)
 
+    # Values are average returns
     for state in states:
         values[state] = np.array(returns[state]).mean()
 
--- chapter5/fig5_2.py
+++ chapter5/fig5_2.py
@@ -10,34 +10,61 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# Blackjack simulator is the same as in figure 5.1
 from fig5_1 import BlackJack
 
 
 def generate_episode(policy):
+    """Single episode simulation
+
+    :param policy: Player's policy. Policy is a dictonary with keys being the player's states
+    and values being True or False for Hit and Stick respectively
+    :type policy: dict
+    :return: Sequence of state-actions and rewards
+    :rtype: tuple
+    """
+
+    # Initialize episode
     black_jack = BlackJack()
     player_state = black_jack.initial_player_state
     action = random.choice((True, False))
     state_action_sequence = [(player_state, action)]
 
+    # Player allways hits below 12
     while black_jack.player_sum < 12:
         player_state = black_jack.player_hits()
         state_action_sequence = [(player_state, action)]
 
+    # Player's move
     while action:
         player_state = black_jack.player_hits()
 
+        # Stop the game if the player goes bust
         if black_jack.player_sum > 21:
             return state_action_sequence, black_jack.rewards[black_jack.player_sum, black_jack.dealer_sum]
 
+        # Add new state to sequence only if the player hadn't gone bust
         action = policy[player_state]
         state_action_sequence.append((player_state, action))
 
+    # Dealer's move
     black_jack.play_as_dealer()
 
     return state_action_sequence, black_jack.rewards[black_jack.player_sum, black_jack.dealer_sum]
 
 
 def plot_policy_values(policy, values):
+    """Plot policy and values heatmap
+
+    :param policy: Policy
+    :type policy: dict
+    :param values: Values
+    :type values: dict
+    :return: None
+    :rtype: None
+    """
+
+    # Convert dicts to numpy arrays
     values_arr = np.zeros((2, 10, 10))
     policy_arr = np.zeros((2, 10, 10))
     for x, y, z in values.keys():
@@ -47,12 +74,15 @@ def plot_policy_values(policy, values):
         values_arr[x, y, z] = value
         policy_arr[x, y, z] = action
 
+    # The sequence of the arrays to be plotted
     arrays = (policy_arr[1, :, :],
               values_arr[1, :, :],
               policy_arr[0, :, :],
               values_arr[0, :, :],)
 
+    # Labels and titles, mathtext for pi anf v
     ylabels = ["Usable ace\nPlayer sum", "Player sum", "No usable ace\nPlayer sum", "Player sum"]
+    plt.rc('mathtext', fontset="cm")
     titles = [r'$\pi_*$', r'$v_*$', '', '']
 
     _, axes = plt.subplots(2, 2, figsize=(12, 9))
@@ -70,18 +100,23 @@ def plot_policy_values(policy, values):
         subplot.set_xlabel("Dealer showing")
         subplot.set_title(titles[i])
 
-    plt.rc('mathtext', fontset="cm")
     plt.show()
 
 
 def monte_carlo_exploring_starts():
+    """Implementation of the Monte Carlo ES (Exploring Starts), page 99. Calculating of the average return
+    change to incremental with respect of exercise 5.4"""
+
+    # Initialize
     usable_ace = (True, False)
     player_sums = range(12, 22)
     dealer_showing = range(1, 11)
     states = tuple(itertools.product(usable_ace, player_sums, dealer_showing))
+    # True for Hit, False for Stick
     actions = (True, False)
 
     state_actions = tuple(itertools.product(states, actions))
+    # Count state-action occurences for incremental average calculation
     state_action_counts = dict((state, 0) for state in state_actions)
     q = dict((state_action, 0) for state_action in state_actions)
     policy = dict(((x, y, z), False) if y in (20, 21) else ((x, y, z), True) for (x, y, z) in states)
@@ -89,14 +124,24 @@ def monte_carlo_exploring_starts():
     t0 = time.perf_counter()
     episodes_number = int(5e5)
 
+    # Loop over episodes
     for i in range(episodes_number):
         print('\r', f'Episode {i + 1}', end='')
         state_action_sequence, reward = generate_episode(policy)
+
+        # Loop for each step of episode
         for (state, action) in state_action_sequence:
+
+            # Update counter
             state_action_counts[state, action] += 1
+
+            # Incremental average
             q[state, action] += (reward - q[state, action]) / state_action_counts[state, action]
+
+            # If q for hit (True) is greater, then choose to hit (True), otherwise choose to stick (False)
             policy[state] = q[state, True] > q[state, False]
 
+    # Get state-values from action-values
     values = dict()
     for player_state in states:
         action = policy[player_state]
